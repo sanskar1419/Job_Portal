09ef37f31e872b63f93a6fe4a89fa1d352599373
SANSKAR GUPTA <sanskarguptabaisali@gmail.com>
2024-01-09 19:43:25 +0530

    Setup express ejs layout and fix header

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ import express from "express";
 import path from "path";
 import HomeController from "./src/controller/home.controller.js";
 import JobsController from "./src/controller/jobs.controller.js";
+import expressEjsLayouts from "express-ejs-layouts";
 
 const app = new express();
 
 app.set("view engine", "ejs");
 app.set("views", path.join(path.resolve(), "src", "views"));
+app.use(expressEjsLayouts);
 
 app.use(express.static("src/views"));
 app.use(express.static("public"));
