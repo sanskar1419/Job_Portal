5db7f81f5006c589ef78a5d9e25a6784bfeefc00
SANSKAR GUPTA <sanskarguptabaisali@gmail.com>
2024-01-09 11:00:20 +0530

    Render home page

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 import express from "express";
+import path from "path";
 
 const app = new express();
+
+app.use(express.static("src/views"));
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(path.resolve(), "src", "views", "home.html"));
+});
 
 export default app;
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import app from "./index.js";
 
-const port = 6000;
+const port = 9000;
 
 app.listen(port, () => {
   console.log(`Server is listing on port :: ${port}`);
